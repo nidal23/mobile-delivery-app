6c2656d5d794e3de385485d19bee04c411d9c943
Nidal Mohammed <nidalmo23@gmail.com>
2023-05-25 02:37:45 +0530

    added home screen

---
+++ screens/HomeScreen.js
@@ -0,0 +1,73 @@
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  TextInput,
+  ScrollView,
+} from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import {
+  UserIcon,
+  ChevronDownIcon,
+  SparklesIcon,
+  AdjustmentsIcon,
+} from "react-native-heroicons/outline";
+import Categories from "../components/Categories";
+
+const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+  return (
+    <SafeAreaView className="bg-white pt-5">
+      {/* header */}
+      <View className="flex-row pb-3 items-center mx-4 space-x-2 ">
+        <Image
+          source={{
+            uri: "https://links.papareact.com/wru",
+          }}
+          className="h-7 b-7 bg-gray-500 p-4 rounded-full"
+        />
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+          <Text className="font-bold text-xl ">
+            Current Location
+            <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
+        </View>
+
+        <UserIcon size={35} color="#00CCBB" />
+      </View>
+      {/* Search */}
+      <View className="flex-row items-center space-x-2 pb-2 mx-4 ">
+        <View className="flex-row flex-1 space-x-2  bg-gray-200 p-3">
+          <SparklesIcon color="gray" size={20} />
+          <TextInput
+            placeholder="Restraunts and cusines"
+            keyboardType="default"
+          />
+        </View>
+        <SparklesIcon color="#00CCBB" />
+      </View>
+      {/* Body */}
+      <ScrollView
+        className="bg-gray-100"
+        contentContainerStyle={{
+          paddingBottom: 100,
+        }}
+      >
+        {/* Categories */}
+        <Categories />
+        {/* Featured Rows */}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
